6b99268e53dc031980b9d36ebd87ccac038686ea
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-03-25 11:36:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -18,7 +18,20 @@
   <h1 class="content">Règles</h1>
   <section class="content">
     <p>3 sous énigmes sur différentes pages une énigme principale une page de présentation + annexes</p>
-    <?php echo "php" ?>
+    <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    ?>
   </section>
 </body>
 </html>

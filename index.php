4ec107ef9f6ec2c7d968b2b95f0701aa1d653c2b
aironebzh <33606430+lce987@users.noreply.github.com>
2018-03-26 00:03:50 +0200

    form

--- index.php
+++ index.php
@@ -25,12 +25,35 @@
     {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
+    // check POST form
+    if($_POST["nom"] || $_POST["prenom"]){
+        $res = mysqli_query($con,'INSERT INTO users (Nom,prenom) VALUES '."(\"". $_POST["nom"]. "\",\"". $_POST["prenom"]."\")" );
+        
+    }
 
     // Perform queries
     $res = mysqli_query($con,"SELECT * FROM users");
-    var_dump($res);
+
+    if ($res->num_rows > 0) {
+      echo "<table> <tr><th>Nom</th><th>Prenom</th>";
+        // output data of each row
+        while($row = $res->fetch_assoc()) {
+            echo "<tr><td>".$row["Nom"]."</td><td>" . $row["prenom"]. "</td></tr><br>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
     mysqli_close($con);
     ?>
+  </section>
+  <section class="content">
+    <form class="user" action="index.php" method="post">
+      <input type="text" name="nom" placeholder="Nom">
+      <input type="text" name="prenom" placeholder="Votre prenom">
+      <input type="submit">
+    </form>
   </section>
 </body>
 </html>

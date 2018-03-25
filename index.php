89157a09279be990cfcae818c22544887ab09178
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-03-25 11:53:57 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 
     // Check connection
     if ($conn->connect_error) {
+      echo "not $conn->connect_error";
       die("Connection failed: " . $conn->connect_error);
     }
     echo "Connected successfully";

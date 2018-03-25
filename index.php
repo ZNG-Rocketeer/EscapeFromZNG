1673bdde2236e7df7b97e5a08788d524229c7063
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-03-25 11:16:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>EscapeFromZNG</title>
+  <meta charset ="utf8">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <section class="image">
+    <img src="img/hack.gif" alt="GIF Hack">
+    <div class="centered">
+      EscapeFromZNG
+      <a href="enigme1.html" class="bouton">
+        Jouer
+      </a>
+    </div>
+  </section>
+  <h1 class="content">Règles</h1>
+  <section class="content">
+    <p>3 sous énigmes sur différentes pages une énigme principale une page de présentation + annexes</p>
+    <?php echo "php" ?>
+  </section>
+</body>
+</html>

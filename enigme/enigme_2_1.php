82a1b4ab79f272e8a3b420c1369bfea46719623c
aironebzh <33606430+lce987@users.noreply.github.com>
2018-04-21 15:45:51 +0200

    css

--- enigme/enigme_2_1.php
+++ enigme/enigme_2_1.php
@@ -3,7 +3,7 @@
 <title>EscapeFromZNG</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="assets/css/master.css">
+<link rel="stylesheet" href="../assets/css/master.css">
 
 <body>
 

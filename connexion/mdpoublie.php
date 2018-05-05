3e9af1b7c7478545307c32550f8b035f7621a2ca
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-05-05 18:25:51 +0200

    page html mdpoublie

--- connexion/mdpoublie.php
+++ connexion/mdpoublie.php
@@ -11,23 +11,31 @@ session_start();
 <body>
   <?php
   include '../assets/php/gen_nav.php';
-  include '../assets/php/pdo_users.php';
-
-  if(isset($_GET['mail'])){
-    // selection user by mail
-    $gmail->execute($_GET['mail']);
-    // gen mdp aleatoire ->
-    $res = $log->fetchAll();
-    if ($log->rowCount()!=0) {
-      $user = $res[0];
-      $to = $user['mail'];
-      $subject = "ZNG CONTACT";
-
-    }
-    else{
-      echo "Vous n'êtes pas encore inscrit sur notre site";
-    }
-  }
   ?>
+  <div id="card" class="zng-center zng-margin-top">
+    <div class="zng-solo">
+      <div  class="zng-card" style="min-height:500px">
+        <h2>Bienvenue</h2>
+        <?php
+        include '../assets/php/pdo_users.php';
+
+        if(isset($_GET['mail'])){
+          // selection user by mail
+          $gmail->execute($_GET['mail']);
+          // gen mdp aleatoire ->
+          $res = $log->fetchAll();
+          if ($log->rowCount()!=0) {
+            $user = $res[0];
+            $to = $user['mail'];
+            $subject = "ZNG CONTACT";
+          }
+          else{
+            echo "Vous n'êtes pas encore inscrit sur notre site";
+          }
+        }
+        ?>
+      </div>
+    </div>
+  </div>
 </body>
 </html>

245de63d6a640ca3f1e780f7c6dfd57e17387a15
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-05-06 11:00:30 +0200

    mdpo

--- connexion/mdpoublie.php
+++ connexion/mdpoublie.php
@@ -11,35 +11,40 @@ session_start();
 <body>
   <?php
   include '../assets/php/gen_nav.php';
+  echo "";
   ?>
   <div id="card" class="zng-center zng-margin-top">
     <div class="zng-solo">
       <div  class="zng-card" style="min-height:500px">
         <h2>Bienvenue</h2>
         <?php
-        echo "test";
         include '../assets/php/pdo_users.php';
-        echo "test";
-
         if(isset($_GET['mail'])){
+          $mail=$_GET['mail'];
           // selection user by mail
-          $gmail->execute($_GET['mail']);
+          $gmail->execute(array($mail));
           // gen mdp aleatoire ->
-          $res = $log->fetchAll();
-          if ($log->rowCount()!=0) {
+          $res = $gmail->fetchAll();
+          if ($gmail->rowCount()!=0) {
             $user = $res[0];
-            $to = $user['mail'];
+            $to = $user['pseudo']."<".$mail.">";
             $subject = "ZNG CONTACT";
+            include '../assets/php/gen_mail.php';
             echo $body;
+            include '../assets/php/gmail.php';
+            
           }
           else{
             echo "Vous n'êtes pas encore inscrit sur notre site";
           }
         }else{
           echo "else";
-
         }
         ?>
+        <form class="zng-form" action="" method="post">
+          <input type="text" name="mail">
+          <input type="submit" value="Envoyer">
+        </form>
       </div>
     </div>
   </div>

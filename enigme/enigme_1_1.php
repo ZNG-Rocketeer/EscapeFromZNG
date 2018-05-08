d2f4a779a52cea07d682e6cd55887a4a92237712
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-05-08 15:48:24 +0200

    chrono

--- enigme/enigme_1_1.php
+++ enigme/enigme_1_1.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../assets/css/master.css">
-
+<script src="../assets/js/chrono.js" charset="utf-8"></script>
 <body>
 
   <?php include '../assets/php/gen_nav.php';

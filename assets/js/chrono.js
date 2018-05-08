afa7c0e697dd38e83f10012e3d51393c05274d85
Erwan <erwan.lecornec@etu.univ-st-etienne.fr>
2018-05-08 17:30:34 +0200

    js

--- assets/js/chrono.js
+++ assets/js/chrono.js
@@ -7,7 +7,7 @@ function chrono(){
 	end = new Date()
 	diff = end - start
 	diff = new Date(diff)
-	var msec = diff.getMilliseconds()
+
 	var sec = diff.getSeconds()
 	var min = diff.getMinutes()
 	var hr = diff.getHours()-1
@@ -17,41 +17,16 @@ function chrono(){
 	if (sec < 10){
 		sec = "0" + sec
 	}
-	if(msec < 10){
-		msec = "00" +msec
-	}
-	else if(msec < 100){
-		msec = "0" +msec
-	}
-	document.getElementById("chronotime").innerHTML = hr + ":" + min + ":" + sec + ":" + msec
+
+	document.getElementById("chronotime").innerHTML = hr + ":" + min + ":" + sec
 	timerID = setTimeout("chrono()", 10)
 }
+
 function chronoStart(){
-	document.chronoForm.startstop.value = "stop!"
-	document.chronoForm.startstop.onclick = chronoStop
-	document.chronoForm.reset.onclick = chronoReset
 	start = new Date()
 	chrono()
 }
-function chronoContinue(){
-	document.chronoForm.startstop.value = "stop!"
-	document.chronoForm.startstop.onclick = chronoStop
-	document.chronoForm.reset.onclick = chronoReset
-	start = new Date()-diff
-	start = new Date(start)
-	chrono()
-}
-function chronoReset(){
-	document.getElementById("chronotime").innerHTML = "0:00:00:000"
-	start = new Date()
-}
-function chronoStopReset(){
-	document.getElementById("chronotime").innerHTML = "0:00:00:000"
-	document.chronoForm.startstop.onclick = chronoStart
-}
+
 function chronoStop(){
-	document.chronoForm.startstop.value = "start!"
-	document.chronoForm.startstop.onclick = chronoContinue
-	document.chronoForm.reset.onclick = chronoStopReset
 	clearTimeout(timerID)
 }
